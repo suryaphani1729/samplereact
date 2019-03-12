1dd535832a10f9abf162d8c2e893af2591737245
surya <surya.phani1729@gmail.com>
2019-03-12 19:38:05 +0530

    Add files via upload

---
+++ precache-manifest.52b484ad1135e9e400876516109b844f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samplereact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d2d00c159f76ca59cd02",
+    "url": "/samplereact/static/js/runtime~main.bacbd486.js"
+  },
+  {
+    "revision": "bc7a9579693cc37b9552",
+    "url": "/samplereact/static/js/main.8640bc52.chunk.js"
+  },
+  {
+    "revision": "c7039e4e58f2bdd72e50",
+    "url": "/samplereact/static/js/2.6e6c3edd.chunk.js"
+  },
+  {
+    "revision": "bc7a9579693cc37b9552",
+    "url": "/samplereact/static/css/main.00255345.chunk.css"
+  },
+  {
+    "revision": "f5e0f2812ffc1b15b355e6524f942b42",
+    "url": "/samplereact/index.html"
+  }
+];
